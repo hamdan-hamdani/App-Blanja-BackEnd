73f548dcadf9527f84dfa1d1a2969b944c3e29ac
Hamdan <hamdan230598@gmail.com>
2020-09-10 15:26:59 +0700

    menambah /categories

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const user = require('./user')
+const category = require('./category')
 
 router
 .use('/users', user)
+.use('/categories', category)
 
 module.exports = router
