f4529fc6b285ca70bb35ba31a01d97926eaf5376
Hamdan <hamdan230598@gmail.com>
2020-09-12 18:13:36 +0700

    tambah file updateuser.js

---
+++ src/routes/updateUser.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const updateUserController = require('../controllers/updateUser')
+const router = express.Router()
+// const { limitFile, verifyAccess } = require('../middlewares/middlewares')
+const { upload, verifyAccess } = require('../middlewares/middlewares')
+const multer = require('multer')
+const jwt = require('jsonwebtoken')
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     console.log(req.body.nameCategory)
+//     console.log('ak multer')
+//     cb(null, `./uploads/imgUser/${req.body.roleId}`)
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now() + file.originalname)
+//   }
+// })
+
+// const upload = multer({     
+//   storage: storage
+// })
+
+router
+  .get('/user/', verifyAccess, updateUserController.getUser)
+  .patch('/user/', verifyAccess, upload.single('image'), updateUserController.updateUser)
+  .get('/address/', verifyAccess, updateUserController.getAddress)
+  .patch('/address/', verifyAccess, updateUserController.updateAddress)
+  .post('/address/', verifyAccess, updateUserController.insertAddress)
+  .get('/store/', verifyAccess, updateUserController.getStore)
+  .patch('/store/', verifyAccess, upload.single('image'), updateUserController.updateStore)
+module.exports = router
